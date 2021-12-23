047666c674c2fddcfe67782d668cb15862b9d8da
hanseltertiusJCU <hansel.tertius@my.jcu.edu.au>
2021-12-23 20:13:54 +0700

    Successfully create loading data widget

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,6 @@ class _ArticleListState extends State<ArticleListWidget> {
 
         articles.add(articleItem);
       }
-
       return articles;
     } else {
       return null;
@@ -80,9 +79,30 @@ class _ArticleListState extends State<ArticleListWidget> {
       body: FutureBuilder(
         future: getArticlesData(),
         builder: (BuildContext context, AsyncSnapshot snapshot) {
-          return const Text('We try to test loading data from server');
+          return const LoadingDataWidget();
         },
       ),
     );
+  }
+}
+
+class LoadingDataWidget extends StatelessWidget {
+  const LoadingDataWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text('Retrieving News Data')),
+            LinearProgressIndicator(
+              value: null,
+            )
+          ],
+        ));
   }
 }
