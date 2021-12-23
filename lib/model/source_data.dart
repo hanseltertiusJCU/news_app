99de509df2fe65d9ada7ad913af100f5fc60fdb7
hanseltertiusJCU <hansel.tertius@my.jcu.edu.au>
2021-12-23 18:08:27 +0700

    Create source_data.dart

---
+++ lib/model/source_data.dart
@@ -0,0 +1,14 @@
+class SourceData {
+  // ignore: slash_for_doc_comments
+  /**
+   * {
+        "id": null,
+        "name": "Mirror Online"
+      }
+   */
+
+  final int? id;
+  final String? name;
+
+  SourceData({this.id, this.name});
+}
