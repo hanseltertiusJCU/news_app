9ae8d4d4eb63ca5768205718a19d256550c2b850
hanseltertiusJCU <hansel.tertius@my.jcu.edu.au>
2021-12-24 15:21:05 +0700

    Update obtaining articles data to be start from today's date

--- lib/functions/get_articles_data.dart
+++ lib/functions/get_articles_data.dart
@@ -1,14 +1,19 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:intl/intl.dart';
 import 'package:news_app/model/response_model.dart';
 import 'package:http/http.dart' as http;
 
 Future<ResponseModel> getArticlesData(http.Client client) async {
   try {
+    var now = DateTime.now();
+    var formatter = DateFormat('yyyy-MM-dd');
+    String formattedDate = formatter.format(now);
+
     var queryParameters = {
       'q': 'tesla',
-      'from': '2021-11-23',
+      'from': formattedDate,
       'sortBy': 'publishedAt',
       'apiKey': '88bba294faa442e5b91e3835a27ce014'
     };
@@ -17,8 +22,6 @@ Future<ResponseModel> getArticlesData(http.Client client) async {
         .get(Uri.https('newsapi.org', 'v2/everything', queryParameters));
 
     var jsonData = jsonDecode(response.body);
-
-    // List<ArticleItem> articles = [];
 
     if (jsonData['status'] == 'ok') {
       return ResponseModel.fromJson(jsonData);
