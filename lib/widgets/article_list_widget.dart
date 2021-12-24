4f5da0a07b4ab4705f0f8b3a49532f2aede29bf2
hanseltertiusJCU <hansel.tertius@my.jcu.edu.au>
2021-12-24 13:00:51 +0700

    Move Widget that shows error item into separate file

--- lib/widgets/article_list_widget.dart
+++ lib/widgets/article_list_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/functions/get_articles_data.dart';
 import 'package:news_app/widgets/article_list_view_widget.dart';
+import 'package:news_app/widgets/error_item_widget.dart';
 import 'package:news_app/widgets/loading_data_widget.dart';
 
 class ArticleListWidget extends StatefulWidget {
@@ -23,27 +24,7 @@ class _ArticleListState extends State<ArticleListWidget> {
           if (snapshot.hasData) {
             return ArticleListViewWidget(snapshot: snapshot);
           } else if (snapshot.hasError) {
-            return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(bottom: 4.0),
-                              child: Text('${snapshot.error}'),
-                            ),
-                            ElevatedButton(
-                                onPressed: () => getArticlesData(),
-                                child: const Text('Try Again'))
-                          ],
-                        ),
-                      )
-                    ]));
+            return ErrorItemWidget(snapshot: snapshot);
           }
 
           return const LoadingDataWidget();
---
+++ lib/widgets/error_item_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/functions/get_articles_data.dart';
+
+class ErrorItemWidget extends StatelessWidget {
+  final AsyncSnapshot snapshot;
+
+  const ErrorItemWidget({Key? key, required this.snapshot}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 4.0),
+                      child: Text('${snapshot.error}'),
+                    ),
+                    ElevatedButton(
+                        onPressed: () => getArticlesData(),
+                        child: const Text('Try Again'))
+                  ],
+                ),
+              )
+            ]));
+  }
+}
